5ccb7c0ac7d4e53bdcd1ddbe6ee00aec2e94ef1e
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-09-15 22:20:04 -0300

    Adiciona arquivos iniciais do projeto

---
+++ src/contacts.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class Contacts extends Component {
+    render() {
+        return (
+            <ScrollView>
+                <View style={ styles.container }>
+
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
