f9beced4874f8166decc753e0d9f44a721f407f2
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-09-16 23:08:57 -0300

    Adiciona estilo da tela de adicionar contatos

--- src/addContact.js
+++ src/addContact.js
@@ -37,27 +37,32 @@ export default class AddContact extends Component {
                     <TextInput 
                         placeholder='Nome' 
                         value={ this.state.name }
-                        onChangeText={ (name) => this.setState({ name }) } />
+                        onChangeText={ (name) => this.setState({ name }) }
+                        style={ styles.input } />
                     
                     <TextInput 
                         placeholder='Telefone' 
                         value={ this.state.phone }
-                        onChangeText={ (phone) => this.setState({ phone }) } />
+                        onChangeText={ (phone) => this.setState({ phone }) }
+                        style={ styles.input } />
                         
                     <TextInput 
                         placeholder='E-mail' 
                         value={ this.state.email }
-                        onChangeText={ (email) => this.setState({ email }) } />
+                        onChangeText={ (email) => this.setState({ email }) }
+                        style={ styles.input } />
                         
                     <TextInput 
                         placeholder='Descrição' 
                         value={ this.state.description }
                         onChangeText={ (description) => this.setState({ description }) } 
-                        multiline />
+                        multiline
+                        style={ styles.input } />
                     
                     <View>
                         <Button 
                             title='Adicionar contato'
+                            color='#593196'
                             onPress={ () => this.addContact() } />
                     </View>
                 </View>
@@ -70,5 +75,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        padding: 16
+    },
+    input: {
+        marginBottom: 16,
+        padding: 8,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: '#593196'
     },
 });
