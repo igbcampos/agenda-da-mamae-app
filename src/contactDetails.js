47bf1c5b5e85ec252aaf2deb5871e7149de2c536
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-09-16 23:06:34 -0300

    Adiciona estilo na tela de detalhes do contato

--- src/contactDetails.js
+++ src/contactDetails.js
@@ -47,27 +47,32 @@ export default class ContactDetails extends Component {
                     <TextInput 
                         placeholder='Nome' 
                         value={ this.state.name }
-                        onChangeText={ (name) => this.setState({ name }) } />
+                        onChangeText={ (name) => this.setState({ name }) }
+                        style={ styles.input } />
                     
                     <TextInput 
                         placeholder='Telefone' 
                         value={ this.state.phone }
-                        onChangeText={ (phone) => this.setState({ phone }) } />
+                        onChangeText={ (phone) => this.setState({ phone }) }
+                        style={ styles.input } />
                         
                     <TextInput 
                         placeholder='E-mail' 
                         value={ this.state.email }
-                        onChangeText={ (email) => this.setState({ email }) } />
+                        onChangeText={ (email) => this.setState({ email }) }
+                        style={ styles.input } />
                         
                     <TextInput 
                         placeholder='Descrição' 
                         value={ this.state.description }
                         onChangeText={ (description) => this.setState({ description }) } 
-                        multiline />
+                        multiline
+                        style={ styles.input } />
                     
                     <View>
                         <Button 
                             title='Atualizar contato'
+                            color='#593196'
                             onPress={ () => this.updateContact() } />
                     </View>
                 </View>
@@ -80,5 +85,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        padding: 16
+    },
+    input: {
+        marginBottom: 16,
+        padding: 8,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: '#593196'
     },
 });
