c07a1125b95748b7e86a352916a3aff77b8e2688
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-09-16 23:16:45 -0300

    Move as configurações do firebase

--- App.js
+++ App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import Firebase from 'firebase';
-
-var firebaseConfig = {
-  apiKey: "AIzaSyAu8_xl_qscGIz7RLPO-j9hRsr4vFI29I8",
-  authDomain: "agenda-da-mamae.firebaseapp.com",
-  databaseURL: "https://agenda-da-mamae.firebaseio.com",
-  projectId: "agenda-da-mamae",
-  storageBucket: "agenda-da-mamae.appspot.com",
-  messagingSenderId: "502785445712",
-  appId: "1:502785445712:web:4bb1a8cc904fc036"
-};
-
-Firebase.initializeApp(firebaseConfig);
+import firebase from 'firebase';
 
 import Contacts from './src/contacts';
 import ContactDetails from './src/contactDetails';
 import AddContact from './src/addContact';
+import Util from './src/util';
+
+firebase.initializeApp(Util);
 
 const NavigationStack = createStackNavigator(
   {
